98b694d381811f47e0913844b1354c31400e8fa7
alexmcgilvery <morrisonjames123@hotmail.com>
2024-05-26 13:54:26 -0400

    identify virtual func overloads

--- rts/newRendering/GL/GLRendererCore.h
+++ rts/newRendering/GL/GLRendererCore.h
@@ -1,4 +1,3 @@
-#pragma once
 
 #include "Rendering/GlobalRendering.h"
 #include "System/type2.h"
@@ -11,25 +10,27 @@ public:
 	CGLRendererCore();
 	~CGLRendererCore();
 
-	void RendererPreWindowInit();
-	void RendererPostWindowInit();
-	bool RendererCreateWindow(const char* title);
-	SDL_Window* RendererCreateSDLWindow(const char* title);
-	void RendererSetStartState();
-	void RendererDestroyWindow();
-	void RendererUpdateWindow();
-	void RendererPresentFrame(bool allowSwapBuffers, bool clearErrors);
+	void RendererPreWindowInit() override;
+	void RendererPostWindowInit() override;
+	bool RendererCreateWindow(const char* title) override;
+	SDL_Window* RendererCreateSDLWindow(const char* title)override;
+	void RendererSetStartState() override;
+	void RendererDestroyWindow() override;
+	void RendererUpdateWindow() override;
+	void RendererPresentFrame(bool allowSwapBuffers, bool clearErrors) override;
 
-	void UpdateViewport();
-	void SetTimeStamp(uint32_t queryIdx) const;
-	uint64_t CalculateFrameTimeDelta(uint32_t queryIdx0, uint32_t queryIdx1) const;
+public:
+	void UpdateViewport() override;
+	void SetTimeStamp(uint32_t queryIdx) const override;
+	uint64_t CalculateFrameTimeDelta(uint32_t queryIdx0, uint32_t queryIdx1) const override;
 
-	bool ToggleDebugOutput(unsigned int msgSrceIdx, unsigned int msgTypeIdx, unsigned int msgSevrIdx) const;
+	bool ToggleDebugOutput(unsigned int msgSrceIdx, unsigned int msgTypeIdx, unsigned int msgSevrIdx) const override;
 
 	// Only one thread can be bound to OpenGL context
-	void AquireThreadContext();
-	void ReleaseThreadContext();
+	void AquireThreadContext() override;
+	void ReleaseThreadContext() override;
 
+public:
 	static void InitStatic();
 	static void KillStatic();
 
